67f9f3304884b2fb55b57e35aa3eeb1ff09a31df
seansamu <seansamu.code@gmail.com>
2025-02-05 02:26:33 -0500

    finished base etch game, extra content can be added later

--- src/0.2_etchsketch/main.js
+++ src/0.2_etchsketch/main.js
@@ -1,28 +1,55 @@
-let gridsize = 16 * 16
-
-const display = document.querySelector('.container')
-const grid_size = document.querySelector('.sizing')
-
-for (let i = 0; i < gridsize; i++) {
-    box = document.createElement('div');
-
-    box.classList.add('hover')
-    display.appendChild(box);
+function styling(box, squares) {
+    size = Math.ceil((960/squares)) - 2
+    box.style.height = size+"px"
+    box.style.width = size+"px"
+    box.style.border = "1px solid black"
 }
 
-// let boxList = document.querySelectorAll('.hover')
-let boxList = document.querySelectorAll('.hover')
-boxList.forEach(boxes => boxes.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = "black"
-}))
-
-display.addEventListener("mouseleave", () => {
+function clearCheck() {
+    let boxList = document.querySelectorAll('.hover')
     boxList.forEach((boxes) => {
         boxes.style.backgroundColor = ""
+        shade = .2
     })
-})
+}
 
-grid_size.addEventListener("click", () => {
+function createGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+        box = document.createElement('div');
+        styling(box, size)
+        box.classList.add('hover')
+        display.appendChild(box);
+    }
 
-})
+    let boxList = document.querySelectorAll('.hover')
+    boxList.forEach(boxes => boxes.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = "rgba(0, 0, 0, " + shade +")"
+        shade[boxes] += .5
+    }))
+}
 
+function resetGrid() {
+    gridsize = prompt("How many squares on one side? (less than 100)")
+    if (gridsize === undefined || gridsize === "" || gridsize > 100 || gridsize < 0)
+        resetGrid()
+
+    let grid = document.querySelector('.container')
+    while (grid.hasChildNodes())
+        grid.removeChild(grid.firstChild)
+
+    shade = .2
+    createGrid(gridsize)
+}
+
+const display = document.querySelector('.container')
+const changeGrid = document.querySelector('.sizing')
+const clear = document.querySelector('.clear')
+
+let shade = []
+let gridsize = 16
+
+createShadeArr(gridsize)
+createGrid(gridsize)
+
+clear.addEventListener("click", clearCheck)
+changeGrid.addEventListener("click", resetGrid)
